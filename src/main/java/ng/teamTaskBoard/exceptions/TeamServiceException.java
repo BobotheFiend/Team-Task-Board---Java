a35e5e0214e7247e66b9ddee068cb4e69aa41f52
BobotheFiend <cavennnamdi@gmail.com>
2026-09-07 09:26:24 +0100

    updating / refactoring

--- src/main/java/ng/teamTaskBoard/exceptions/TeamServiceException.java
+++ src/main/java/ng/teamTaskBoard/exceptions/TeamServiceException.java
@@ -4,4 +4,13 @@ public class TeamServiceException extends TeamTaskBoardException {
     public TeamServiceException(String message) {
         super(message);
     }
+    public TeamServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public TeamServiceException(){
+        super();
+    }
+    public TeamServiceException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/main/java/ng/teamTaskBoard/exceptions/TeamTaskBoardException.java
+++ src/main/java/ng/teamTaskBoard/exceptions/TeamTaskBoardException.java
@@ -4,4 +4,13 @@ public class TeamTaskBoardException extends Exception {
     public TeamTaskBoardException(String message) {
         super(message);
     }
+    public TeamTaskBoardException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public TeamTaskBoardException(){
+        super();
+    }
+    public TeamTaskBoardException(Throwable cause) {
+        super(cause);
+    }
 }
